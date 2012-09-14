5f4cdbb6ceb6671f1b95992066e68d49a7f78688
Lauren Middleton <git@laurenmiddleton.com>
2012-09-13 22:36:26 -0400

    MB test

--- index.php
+++ index.php
@@ -5,3 +5,5 @@ Now we are testing a workflow runthrough.
 We are testing again.
 
 Now we are testing to see if we can make changes on the iMac.
+
+Now we are testing to see if we can make changes on the MacBook.

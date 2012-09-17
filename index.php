94b19bce4fd92c3bb8524ae3a54bb1ee1b18b036
Lauren Middleton <git@laurenmiddleton.com>
2012-09-17 13:08:32 -0400

    cerro-vista test

--- index.php
+++ index.php
@@ -7,3 +7,5 @@ We are testing again.
 Now we are testing to see if we can make changes on the iMac.
 
 Now we are testing to see if we can make changes on the MacBook.
+
+Now we are doing one more test to see if we can make changes from cerro-vista.

0e5b4fe5535eea887ff5010aadcaf11d1165a823
Lauren Middleton <git@laurenmiddleton.com>
2012-11-03 15:33:06 -0400

    removed JS from template

--- p2.laurenmiddleton.com/views/_v_template.php
+++ p2.laurenmiddleton.com/views/_v_template.php
@@ -9,13 +9,6 @@
 	<link rel='stylesheet' type='text/css' href='/css/lkm-styles.css' />
 	
 	<!-- JS -->
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
-	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
-	
-	<script type="text/javascript" src="/js/jquery-ui-1.8.24.custom/js/jquery-1.8.2.min.js"></script>
-	<script type="text/javascript" src="/js/jquery-ui-1.8.24.custom/js/jquery-ui-1.8.24.custom.min.js"></script>
-	<script type="text/javascript" src="../js/p2-main.js"></script>
-	<script type="text/javascript" src="../js/p2-nav-helper-functions.js"></script>	
 				
 	<!-- Controller Specific JS/CSS -->
 	<?php echo @$client_files; ?>

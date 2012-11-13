a5c64a7963d895dd0bca086e431fe2f5f6b10186
Lauren Middleton <git@laurenmiddleton.com>
2012-11-12 20:36:14 -0500

    basic dragging and dropping working w/ custom accordion

--- p3.laurenmiddleton.com/js/p3-main.js
+++ p3.laurenmiddleton.com/js/p3-main.js
@@ -1,29 +1,58 @@
 $(document).ready(function() {
-
-	$("#word-accordion").accordion();
 	
-	counter = 0;
+	$(".accordion-btn").click(function() {
+		//find and store id of the clicked btn
+		var id = $(this).attr("id");
+		//hide open content div
+		$(".accordion-content").hide();
+		//show the content div that corresponds to the clicked btn
+		$("#" + id + "-content").show();
+	});
 	
-	$(".word").draggable( {
-		//appendTo: "#canvas",
+	$(".draggable").draggable( {
 		containment: "#container",
 		cursor: "move",
-		helper: "clone",
-		//snap: true,
-		start: function() {
-			$(this).addClass("dragged");
-		},
 		stop: function() {
-			//$(this).removeClass("word").addClass("word-clone").appendTo("#canvas");
-			$(this).draggable({
+			$(this).draggable( {
 				containment: "#canvas",
 				cursor: "move",
-				stop: function() {
-					$(this).addClass("ui.helper").appendTo("#canvas");
-				},
 			});
 		},
 	});
+	
+	$("#canvas").droppable( {
+		accept: ".draggable",
+		drop: function() {
+			$(".draggable").addClass("dropped").appendTo("#canvas");
+			alert("dropped!");
+		},
+		tolerance: "fit",
+	});
+	
+	//$("#word-accordion").accordion();
+	
+	//counter = 0;
+	
+	//$(".word").draggable( {
+	//	//appendTo: "#canvas",
+	//	containment: "#container",
+	//	cursor: "move",
+	//	helper: "clone",
+	//	//snap: true,
+	//	start: function() {
+	//		$(this).addClass("dragged");
+	//	},
+	//	stop: function() {
+	//		//$(this).removeClass("word").addClass("word-clone").appendTo("#canvas");
+	//		$(this).draggable({
+	//			containment: "#canvas",
+	//			cursor: "move",
+	//			stop: function() {
+	//				$(this).addClass("ui.helper").appendTo("#canvas");
+	//			},
+	//		});
+	//	},
+	//});
 	
 	//$(".word-clone").draggable( {
 	//	containment: "#canvas",
@@ -31,13 +60,13 @@ $(document).ready(function() {
 		
 	//});
 	
-	$("#canvas").droppable( {
-		accept: ".word",
-		drop: function() {
-			$(".dragged").removeClass("word dragged").addClass("word-clone dropped").appendTo("#canvas");
-			alert("dropped!");
-		},
-		tolerance: "fit",
-	});
+	//$("#canvas").droppable( {
+	//	accept: ".word",
+	//	drop: function() {
+	//		$(".dragged").removeClass("word dragged").addClass("word-clone dropped").appendTo("#canvas");
+	//		alert("dropped!");
+	//	},
+	//	tolerance: "fit",
+	//});
 
 }); //end document ready

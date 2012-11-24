dfc30f142ce150023956cd199498aab824092346
Lauren Middleton <git@laurenmiddleton.com>
2012-11-23 22:54:08 -0500

    improved styling

--- p3.laurenmiddleton.com/js/p3-main.js
+++ p3.laurenmiddleton.com/js/p3-main.js
@@ -23,16 +23,13 @@ $(document).ready(function() {
 		var id = $(this).attr("id");
 		//store id of clicked buttons content div
 		var content = "#" + id + "-content";
-		console.log(content);
 		var display = $(content).css("display");
-		console.log(display)
 		//if clicked btn's content isn't open already...
 		if(display = "none") { //IF STATEMENT CURRENTLY BROKEN
 			//hide open content div
 			$(".accordion-content").hide("blind", 1000);
 			//show the content div that corresponds to the clicked btn
 			$("#" + id + "-content").show("blind", 1000);
-			console.log("the if statement was run");
 		}
 	});
 	
